6f5be23ce3481d85cb92b2d895394d7f32cf9cba
AS3-1-09 <AS3-1-09@uni.lan>
2020-01-20 08:15:23 +0100

    dgf

---
+++ JavaScript/Ejercicios/pruebaJSexterno.js
@@ -0,0 +1,6 @@
+<script>
+document.getElementById("myImage").style.display = "none"
+document.getElementById("button1").style.display = "none"
+document.getElementById("button2").style.display = "none"
+document.getElementById("button3").style.display = "none"
+</script>
